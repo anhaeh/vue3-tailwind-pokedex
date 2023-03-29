d269d7ce6d016588b8980c9bcbe66509adfd8573
Andres Haehnel <ahaehnel@kiusys.com>
2023-03-28 21:14:02 -0300

    reduce css

--- src/views/PokedexView.vue
+++ src/views/PokedexView.vue
@@ -37,3 +37,78 @@ watch(filterValue, () => page.value = 1)
       <paginator v-model:page="page" :page-count="pageCount" />
     </div>
 </template>
+
+<style scoped lang="sass">
+:deep()
+  .background-color-bug
+    background-color: #729f3f
+    color: #fff
+
+  .background-color-dark
+    background-color: #707070
+    color: #fff
+
+  .background-color-dragon
+    background-color: #53a4cf
+    color: #fff
+
+  .background-color-electric
+    background-color: #eed535
+    color: #212121
+
+  .background-color-fairy
+    background-color: #fdb9e9
+    color: #212121
+
+  .background-color-fighting
+    background-color: #d56723
+    color: #fff
+
+  .background-color-fire
+    background-color: #fd7d24
+    color: #fff
+
+  .background-color-flying
+    background-color: #3dc7ef
+    color: #212121
+
+  .background-color-ghost
+    background-color: #7b62a3
+    color: #fff
+
+  .background-color-grass
+    background-color: #9bcc50
+    color: #212121
+
+  .background-color-ground
+    background-color: #f7de3f
+    color: #212121
+
+  .background-color-ice
+    background-color: #51c4e7
+    color: #212121
+
+  .background-color-normal
+    background-color: #a4acaf
+    color: #212121
+
+  .background-color-poison
+    background-color: #b97fc9
+    color: #fff
+
+  .background-color-psychic
+    background-color: #f366b9
+    color: #fff
+
+  .background-color-rock
+    background-color: #a38c21
+    color: #fff
+
+  .background-color-steel
+    background-color: #9eb7b8
+    color: #212121
+
+  .background-color-water
+    background-color: #4592c4
+    color: #fff
+</style>
